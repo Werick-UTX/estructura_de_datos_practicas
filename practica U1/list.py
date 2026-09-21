d565b14fa565d09589449f3591cb04c6a4481ab2
Erick Cruz Gonzalez <250780@utxicotepec.edu.mx>
2026-09-21 08:59:00 -0600

    Practica Metodo burbuja
    add:se agregó el ejemplo del metodo burbuja

---
+++ practica U1/list.py
@@ -0,0 +1,11 @@
+# Recorrida de lista 1
+lista = [10,20,30,40,50,60]
+for x in lista:
+    print(x)
+
+#Recorrido de lista 2
+for i in range(len(lista)):
+    print(lista[i])
+    
+print(lista[::]) #Slices
+print(lista[::-1]) #Recorrido completo invertido
